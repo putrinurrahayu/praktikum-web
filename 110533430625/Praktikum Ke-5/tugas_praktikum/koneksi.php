ad0be1d20916d5a357eb326ecccedb2417ff1190
putri nur rahayu <putri_nur_rahayu@yahoo.co.id>
2013-11-07 11:29:01 +0700

    praktikum5
    praktikum5

---
+++ 110533430625/Praktikum Ke-5/tugas_praktikum/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$host = 'localhost'; //nama server
+$user = 'root'; // nama user
+$pass = ''; //password
+$db = 'myweb'; //nama basis data
+$cnn = mysql_connect($host, $user, $pass);
+if (!$cnn) { //kondisi jika koneksi gagal
+exit('Koneksi Gagal');
+}
+$db = mysql_select_db($db);
+if (!$db) { //kondisi jika tidak ada basis data yang terpilih
+exit('Maaf anda gagal menambah basis data');
+}
+?>
